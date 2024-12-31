eccb976e027e5633d2bf51c30f39db27268d406f
Wanderer <82797317+wanderer00a1@users.noreply.github.com>
2024-12-31 13:38:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,81 +4,81 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Product = require('./models/product');
 const methodOverride = require('method-override');
+const appError = require('./appError');
+const { handleAsync } = require('./appError');
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/Blito');
+  await mongoose.connect('mongodb://127.0.0.1:27017/ErrorAsync');
   console.log('Connected to MongoDB');
 }
 
-const categories = ['Grocery','Snacks','Drinks','Personalcare'];
+const categories = ['Grocery', 'Snacks', 'Drinks', 'Personalcare'];
 
-app.set('views',path.join(__dirname, 'views'));
-//Error: Failed to lookup view "products/index" in views directory "D:\Project"
-app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-//middleware part 
-app.use(express.urlencoded({extended: true}))
-// override with POST having ?_method=DELETE
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
-
-app.get('/products',async (req,res) =>{
-  const { category} = req.query;
-  if(category){
-    const products = await Product.find({category});
-    res.render('products/index',{products,categories, category});
-  }
-  else{
+app.get('/products', handleAsync(async (req, res, next) => {
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category });
+    res.render('products/index', { products, categories, category });
+  } else {
     const products = await Product.find({});
-    res.render('products/index',{products,categories,category:'All'});
-  }  
-})
+    res.render('products/index', { products, categories, category: 'All' });
+  }
+}));
 
-//to the page for creating add items
-app.get('/products/new', (req,res) =>{
-  res.render('products/new',{categories});
-})
+app.get('/products/new', (req, res) => {
+  res.render('products/new', { categories });
+});
 
-app.post('/products', async (req,res) =>{
-  console.log(req.body)
+app.post('/products', handleAsync(async (req, res) => {
   const newProduct = new Product(req.body);
-  //have to use app.use(express.urlencoded({extended: true})) to parse that content middleware part
   await newProduct.save();
-  res.redirect(`products/${newProduct._id}`)
-})
+  res.redirect(`products/${newProduct._id}`);
+}));
 
-
-app.get('/products/:id', async (req,res) =>{
-  //:id coz its dynamic path
-  //use the id to fetch a product from the database
-  const {id} = req.params;
+app.get('/products/:id', handleAsync(async (req, res, next) => {
+  const { id } = req.params;
   const product = await Product.findById(id);
-  res.render('products/show',{product});
-})
+  if (!product) {
+    throw new appError('Not found ', 404);
+  }
+  res.render('products/show', { product });
+}));
 
-//has to use method override 
-app.get('/products/:id/edit', async (req,res) =>{
-  const {id} = req.params;
+app.get('/products/:id/edit', handleAsync(async (req, res, next) => {
+  const { id } = req.params;
   const product = await Product.findById(id);
-  res.render('products/edit',{product, categories});
-})
+  if (!product) {
+    throw new appError('Not found ', 404);
+  }
+  res.render('products/edit', { product, categories });
+}));
 
-app.put('/products/:id', async (req,res) =>{
-  const {id } = req.params;
-  const product = await Product.findByIdAndUpdate(id, req.body,{runValidators:true,new:true})
+app.put('/products/:id', handleAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
   res.redirect(`/products/${product._id}`);
-})
+}));
 
-app.delete('/products/:id', async (req,res) =>{
-  const {id} = req.params;
+app.delete('/products/:id', async (req, res) => {
+  const { id } = req.params;
   const delProduct = await Product.findByIdAndDelete(id);
   res.redirect('/products');
-})
+}));
 
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Something Went Wrong' } = err;
+  res.status(status).send(message);
+});
 
-app.listen('3300',() =>{
-    console.log("Server is now Running -------->")
-})
+app.listen('3300', () => {
+  console.log("Server is now Running -------->");
+});
